adb27b98b211807a02afa8d44ac4d5c9ffc33776
ddejesusr <ddejesus2610@gmail.com>
2026-08-13 01:01:41 -0400

    Add storage database configuration

---
+++ src/sra_storage/sra_storage/database.py
@@ -0,0 +1,20 @@
+"""Database configuration for the SRA V2 storage domain."""
+
+import os
+
+import psycopg2
+
+
+DB_CONFIG = {
+    "host": os.getenv("SRA_V2_DB_HOST", "localhost"),
+    "dbname": os.getenv("SRA_V2_DB_NAME", "sra_v2_db"),
+    "user": os.getenv("SRA_V2_DB_USER", "sra_user"),
+    "password": os.getenv("SRA_V2_DB_PASSWORD", ""),
+    "port": int(os.getenv("SRA_V2_DB_PORT", "5432")),
+}
+
+
+def connect_database():
+    connection = psycopg2.connect(**DB_CONFIG)
+    connection.autocommit = False
+    return connection
