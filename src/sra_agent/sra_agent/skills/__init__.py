7a99e74733a54fb51b3bddfdb0f9f6faf59c576e
ddejesusr <ddejesus2610@gmail.com>
2026-08-13 00:31:15 -0400

    Initialize skill package

---
+++ src/sra_agent/sra_agent/skills/__init__.py
@@ -0,0 +1,11 @@
+from .base import Skill, SkillDefinition
+from .inventory import GetStockSkill, GetTotalStockSkill
+from .registry import SkillRegistry
+
+__all__ = [
+    "Skill",
+    "SkillDefinition",
+    "SkillRegistry",
+    "GetStockSkill",
+    "GetTotalStockSkill",
+]
