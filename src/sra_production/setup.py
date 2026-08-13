d62f79ae05869201a387a2b0b6cd3797705ba453
ddejesusr <ddejesus2610@gmail.com>
2026-08-13 00:47:01 -0400

    Add production tracking package

---
+++ src/sra_production/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+package_name = 'sra_production'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='master',
+    maintainer_email='master@todo.todo',
+    description='SRA production tracking and Festo carrier digital-twin logic',
+    license='MIT',
+    entry_points={'console_scripts': []},
+)
